512e48e3dff024382c546c135dd99ef905a7ee32
Ronye Rocha <ronye.rocha@pisomtech.com.br>
2021-09-23 23:19:04 -0300

    initial commit

---
+++ PaitingCalculator.API/Controllers/CalculatorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace PaitingCalculator.API.Controllers
+{
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [Route("calculate")]
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+    }
+}
